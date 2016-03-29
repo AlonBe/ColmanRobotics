e980ef73ac22d4961dcf8f1a04f9cd3e32da7f9c
Alon Berkman <alon.berkman@gmail.com>
2016-03-29 13:47:29 -0700

    Talya's changes

--- HelloRobot/main.cpp
+++ HelloRobot/main.cpp
@@ -1,53 +1,93 @@
 /*
- * main.cpp
- *
- *  Created on: Jan 10, 2015
- *      Author: colman
- */
+* main.cpp
+*
+* Alon Berkman 302444252
+* Talya Ben Haim 205412570
+*
+*/
 
 #include <libplayerc++/playerc++.h>
 #include <iostream>
 #include <stdio.h>
+#include <cmath>
 
 using namespace PlayerCc;
 
-int main() {
-
+int main()
+{
 	PlayerClient pc("localhost", 6665);
 	SonarProxy sp(&pc);
 	Position2dProxy pp(&pc);
-
 	pp.SetMotorEnable(true);
-	while (true) {
+
+	while (true)
+	{
+		double distance, angle;
+
 		pc.Read();
 
-		if (sp[2] < 0.7) {
+		if (sp[2] < 0.7)
+		{
+			angle = 0;
+			distance = sp[2];
+
 			// If both right and left are not the best option
-			if (sp[1] < 0.7 && sp[3] < 0.7) {
+			if (sp[1] < 0.7 && sp[3] < 0.7)
+			{
 				pp.SetSpeed(-0.5, 3);
-				std::cout << "1";
 			}
-			else if (sp[1] < sp[3]) {
+			else if (sp[1] < sp[3])
+			{
 				pp.SetSpeed(0, 0.5);
-				std::cout << "2";
 			}
-			else {
+			else
+			{
 				pp.SetSpeed(0, -0.5);
-				std::cout << "3";
 			}
 		}
-		else if (sp[1] < 0.7 || sp[0] < 0.4) {
+		else if (sp[1] < 0.7)
+		{
+			distance = sp[1];
+			angle = 25;
+
 			pp.SetSpeed(0.1, 0.3);
-			std::cout << "4";
 		}
-		else if (sp[3] < 0.7 || sp[4] < 0.4) {
+		else if(sp[0] < 0.4)
+		{
+			distance = sp[1];
+			angle = 90;
+
+		    pp.SetSpeed(0.1, 0.3);
+		}
+		else if (sp[3] < 0.7)
+		{
+			distance = sp[3];
+			angle = -25;
+
 			pp.SetSpeed(0.1, -0.3);
 			std::cout << "5";
 		}
-		else {
+		else if(sp[4] < 0.4)
+		{
+			distance = sp[3];
+			angle = -90;
+
+			pp.SetSpeed(0.1, -0.3);
+		}
+		else
+		{
+			distance = 0.8;
 			pp.SetSpeed(0.5, 0);
-			std::cout << "6";
+		}
+
+		double positionX = pp.GetXPos() + distance * cos(pp.GetYaw() + angle);
+		double positionY = pp.GetYPos() + distance * sin(pp.GetYaw() + angle);
+
+		if (distance != 0.8)
+		{
+			std::cout << "Obstacle location:(" << positionX << "," << positionY << ")\n";
 		}
 	}
+
 	return 0;
 }
